2a744411f6411c10bdd14cfec050eda0f8e23d6e
balazs burian <bur.balazs@protonmail.com>
2022-11-29 14:50:28 +0100

    added controllers

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,40 @@
+class ContactsController < ApplicationController
+  def index
+    @contacts = Contact.all
+  end
+
+  def show
+    @contact = Contact.find(params[:id])
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    @contact.household_id = current_user.household_id
+    if @contact.save!
+      redirect_to contacts_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @contact = Contact.find(params[:id])
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+    @contact.update(contact_params)
+    redirect_to contacts_path
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :phone, :address)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
+  resources :tasks # Defines the root path route ("/")
   # root "articles#index"
+  resources :contacts, only: [:index, :show, :create, :new]
 end
+
+# resources :categories, only: [:index, :show] ??
+# resources :templates, only: [:index, :show]
+# resources :kids, only [:show, :create]
+# resources :housholds, only [:index]
