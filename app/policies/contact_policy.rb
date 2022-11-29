0c5f980daf96c607acbf5f526d5ea73133bd6dae
balazs burian <bur.balazs@protonmail.com>
2022-11-29 16:49:38 +0100

    pundit is working

--- app/policies/contact_policy.rb
+++ app/policies/contact_policy.rb
@@ -1,26 +1,27 @@
 class ContactPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
-    def show?
-      true
+    def resolve
+      scope.all.where(household: user.household)
     end
-
-    def create?
-      true
-    end
-
-    def update?
-      record.user == user
-      # record: the restaurant passed to the `authorize` method in controller
-      # user: the `current_user` signed in with Devise
-    end
-
-    def destroy?
-      record.user == user
-    end
-    
   end
+
+  def show?
+    true
+  end
+
+  def create?
+    user
+  end
+
+  def update?
+    record.household == user.household
+    # record: the restaurant passed to the `authorize` method in controller
+    # user: the `current_user` signed in with Devise
+  end
+
+  def destroy?
+    record.household == user.household
+  end
+
 end
