152ccc3a4550f984ef9a0e76da93cbdd80d8c4c7
LinusPla <45211685+LinusPla@users.noreply.github.com>
2022-11-30 10:45:20 +0100

    assigned to logic working

--- db/migrate/20221129104706_create_tasks.rb
+++ db/migrate/20221129104706_create_tasks.rb
@@ -9,7 +9,7 @@ class CreateTasks < ActiveRecord::Migration[7.0]
       t.date :due_date
       t.references :kid, foreign_key: true
       t.boolean :status
-      t.string :assigned_to
+      t.references :assigned_to, default: nil, foreign_key: { to_table: :users }
       t.references :category, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
       t.references :household, null: false, foreign_key: true
