d1d45bcbd9d42ba17d898759d44077c50dd3647d
Morgan Cavanagh <cavana.morgan@gmail.com>
2022-12-06 12:32:39 +0100

    Finally the button works

--- app/javascript/controllers/mark_controller.js
+++ app/javascript/controllers/mark_controller.js
@@ -1,8 +1,26 @@
 import { Controller } from "@hotwired/stimulus"
 
+
 // Connects to data-controller="mark"
 export default class extends Controller {
+  static targets = ["submit", "form", "task"]
   connect() {
     console.log("hello")
+  }
+
+  check(event) {
+    // this.formTarget.submit()
+    console.log(event)
+    // event.preventDefault()
+    const url = this.formTarget.action
+  fetch(url, {
+    method: "PATCH",
+    headers: { "Accept": "text/plain" },
+    body: new FormData(this.formTarget)
+  })
+    .then(response => response.text())
+    .then((data) => {
+      this.taskTarget.outerHTML = data
+    })
   }
 }
