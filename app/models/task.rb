20b3ed95a75e4793ef7e9799333078ee2e1667d8
balazs burian <bur.balazs@protonmail.com>
2022-12-06 15:22:34 +0100

    added validations contacts and tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,4 +5,6 @@ class Task < ApplicationRecord
   belongs_to :kid, optional: true
   belongs_to :contact, optional: true
   belongs_to :assigned_to, class_name: "User", optional: true
+  validates :name, presence: true
+  validates :category, presence: true
 end
